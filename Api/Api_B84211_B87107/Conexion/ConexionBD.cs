32313987784b253234569fe25d9319bf4985f1ec
AracelySC <arasaenz15@gmail.com>
2021-07-12 10:36:12 -0600

    Api  conexion a BD

---
+++ Api/Api_B84211_B87107/Conexion/ConexionBD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_B84211_B87107.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_B84211_B87107.Conexion
+{
+    public class ConexionBD : DbContext
+    {
+        public ConexionBD(DbContextOptions<ConexionBD> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> TB_USUARIO { get; set; }
+
+    }
+}
---
+++ Api/Api_B84211_B87107/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_B84211_B87107.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        public int cedula { get; set; }
+        public string contrasena { get; set; }
+    }
+}
+
