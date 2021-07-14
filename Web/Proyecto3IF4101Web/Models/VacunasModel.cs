814e046a1974b75d99e5cbec6012779fb8542642
AracelySC <arasaenz15@gmail.com>
2021-07-14 00:04:28 -0600

    View registrar vacunas y procedimientos almacenados BD

---
+++ Web/Proyecto3IF4101Web/Models/VacunasModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Proyecto3IF4101Web.Models
+{
+    public class VacunasModel
+    {
+        public SelectList VacunasList { get; set; }
+        public SelectList PacientesList { get; set; }
+
+        public int ID_VACUNA { get; set; }
+        public int CEDULA { get; set; }
+
+        public string DECRIPCION { get; set; }
+        public string FECHA_APLI { get; set; }
+        public string DFECHA_PROX { get; set; }
+
+
+    }
+
+    public class Vacunas
+    {
+       public int ID { get; set; }
+       public string NOMBRE { get; set; }
+       public string DECRIPCION { get; set; }
+
+    }
+
+    public class Pacientes
+    {
+        public int CEDULA { get; set; }
+        public string NOMBRE { get; set; }
+
+    }
+
+}
